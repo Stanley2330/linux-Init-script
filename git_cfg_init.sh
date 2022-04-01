933509ca5df5d57bb18bb620ef59957cb3443af6
Stanley <zylizj@tsmc.com>
2022-04-01 10:17:37 +0800

    [Feature]init script
    add apt installl - tig
    add git config & git alias
    add ssh-keygen & show pubkey

---
+++ git_cfg_init.sh
@@ -0,0 +1,8 @@
+git config --global user.name "Stanley"
+git config --global user.email "zylizj@tsmc.com"
+git config --global core.editor vim
+
+git config --global alias.ch checkout
+git config --global alias.br branch
+git config --global alias.st status
+git config --global alias.lg "log --decorate --pretty=oneline --abbrev-commit"
