fb83d168bf2c56238f14d4d510a0130c063ac626
Stanley <zylizj@tsmc.com>
2022-04-01 11:00:23 +0800

    [Feature]add git remote update alias

--- git_cfg_init.sh
+++ git_cfg_init.sh
@@ -5,4 +5,5 @@ git config --global core.editor vim
 git config --global alias.ch checkout
 git config --global alias.br branch
 git config --global alias.st status
+git config --global alias.up "remote update"
 git config --global alias.lg "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
